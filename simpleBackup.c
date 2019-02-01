5539aa68f195723f9dd4773ab66d2af9e5f03cef
Lucas Carvalho Roncoroni <lucascarvalhoroncoroni@Lucass-Mac-mini.local>
2019-02-01 01:02:21 -0200

    Making the raw communication work again

--- simpleBackup.c
+++ simpleBackup.c
@@ -9,8 +9,13 @@
 #include <errno.h>
 #include <string.h>
 #include <sys/socket.h>
-
-#define BUFFER_SIZE 4196 
+#include <dirent.h>
+#include <errno.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <netinet/ip.h>
+#include "simpleBackupConstants.h"
 
 
 /**
@@ -120,9 +125,61 @@ int main(int argc, char* argv[])
     // copyFolder(argv[1], argv[2]);
 
     //tcp socket
-    int connectionSocket = socket(AF_INET, SOCK_STREAM, 0);
-
-
+    
+    int server_fd, new_socket, valread;
+    struct sockaddr_in address;
+    int opt = 1;
+    int addrlen = sizeof(address);
+    char buffer[1024] = {0};
+    char *hello = "Hello from server";
+    struct fileTransfering message;
+    
+    //testing the message
+    message.code = 2456;
+    message.messageSize = 11;
+    sprintf(message.buffer, "0123456789");
+    
+    // Creating socket file descriptor
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+    {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+    
+    // Allowing reuse of IP address
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR ,
+                   &opt, sizeof(opt)))
+    {
+        perror("setsockopt");
+        exit(EXIT_FAILURE);
+    }
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(10015);
+    
+    // Forcefully attaching socket to the port 8080
+    if (bind(server_fd, (struct sockaddr *)&address,
+             sizeof(address))<0)
+    {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+    if (listen(server_fd, 3) < 0)
+    {
+        perror("listen");
+        exit(EXIT_FAILURE);
+    }
+    
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address,
+                             (socklen_t*)&addrlen))<0)
+    {
+        perror("accept");
+        exit(EXIT_FAILURE);
+    }
+    
+    send(new_socket , (void*) &message, sizeof(struct fileTransfering), 0);
+    printf("Hello message sent\n");
+    return 0;
 
     return 0;
 }
